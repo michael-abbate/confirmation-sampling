0152b8949ee422dca446571b4f40e6d64de82cbb
Michael Abbate <mabbate@mabba-NY-1022m.localdomain>
2023-01-22 18:03:44 -0500

    update rules in scopingRec

--- app/scopingreconciliation.py
+++ app/scopingreconciliation.py
@@ -8,15 +8,18 @@ def scopingRec(scope_file1, scope_file2):
     # new_df = pd.merge(df1, df2,  how='inner', on=cols_to_join)
     # new_df = pd.merge(df2,df1, on = cols_to_join) 
     
-    # new_df = df2.loc[(df2[cols_to_join[0]].isin(df1[cols_to_join[0]]))
-    #                 &   (df2[cols_to_join[1]].isin(df1[cols_to_join[1]]))  
-    #                 &   (df2[cols_to_join[2]].isin(df1[cols_to_join[2]]))  
-    # ,:]
-    new_df = df2.merge(df1, how = 'inner', on=cols_to_join, suffixes=('', '_remove'))
+    df2 = df2.loc[(df2[cols_to_join[0]].isin(df1[cols_to_join[0]])),:]
+    df2 = df2.loc[(df2[cols_to_join[1]].isin(df1[cols_to_join[1]])),:]
+    df2 = df2.loc[(df2[cols_to_join[2]].isin(df1[cols_to_join[2]])),:]
+                    # &   (df2[cols_to_join[1]].isin(df1[cols_to_join[1]]))  
+                    # &   (df2[cols_to_join[2]].isin(df1[cols_to_join[2]]))  
     
-    new_df.drop([i for i in new_df.columns if '_remove' in i],
-               axis=1, inplace=True)
+    # new_df = df2.merge(df1, how = 'inner', on=cols_to_join, suffixes=('', '_remove'))
+    
+    # new_df.drop([i for i in new_df.columns if '_remove' in i],
+    #            axis=1, inplace=True)
 
     # new_df = df2.join(df1, on = cols_to_join)
-    print(new_df.to_string())
-    return new_df
+    # print(new_df.to_string())
+    # return new_df
+    return df2
