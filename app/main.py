2363c05d55c4f09d6a5398ae38849506b4471de0
Michael Abbate <mabbate@mabba-NY-1022m.localdomain>
2023-01-22 16:17:43 -0500

    fix bug of no file or directory found error

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from datetime import datetime, timedelta
 import pandas as pd
 
 from flask import (Flask, abort, flash, jsonify, make_response, redirect, render_template, request, session, url_for)
+from werkzeug.utils import secure_filename
+
 # from flask_login import (LoginManager, current_user, login_required, login_user, logout_user)
 
 from app import app
@@ -12,6 +14,10 @@ from app import app
 
 from confirmationsampling import determineCutoffThreshold
 from scopingreconciliation import scopingRec
+
+
+UPLOAD_FOLDER = os.path.join('app/uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 __version__ = '0.0.1'
 __basedir__ = os.path.abspath(os.path.dirname(__file__))
@@ -25,7 +31,11 @@ __rootdir__ = os.path.abspath(os.getcwd())
 # timeout = timedelta(minutes=60)
 # app.permanent_session_lifetime = timeout
 
-
+def removeLocalFile(filepath):
+    if os.path.exists(filepath):
+        os.remove(filepath)
+    else:
+        print("The file does not exist")
 
 # Home route
 @app.route('/', methods = ['GET', 'POST'])
@@ -33,13 +43,32 @@ __rootdir__ = os.path.abspath(os.getcwd())
 def home():
     return render_template('index.html', message= "")
 
+@app.route('/test', methods = ['GET', 'POST'])
+def test():
+    if request.method=="POST":
+        try:
+            file = request.files["file"]
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            df=pd.read_csv(file_path)
+            removeLocalFile(file_path)
+            return render_template("test.html", df=df.to_html())
+        except Exception as e:
+            flash(f"Error, {e}", 'error')
+    return render_template('test.html', df= None)
+
 @app.route('/sampling', methods = ['GET', 'POST'])
 def sampling():
     if request.method=="POST":
         try:
             file = request.files["file"]
             outputpath = request.form["outputpath"]
-            df=pd.read_csv(file.filename)
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            df=pd.read_csv(file_path)
+            removeLocalFile(file_path)
             data_to_save=determineCutoffThreshold(df)
             for data in data_to_save:
                 # Update outputs folder to be an option for user
@@ -58,17 +87,25 @@ def sampling():
 def scoping():
     if request.method=="POST":
         try:
-            file_1 = request.files["file_1"]
+            file_1 = request.files['file_1']
+            filename_1 = secure_filename(file_1.filename)
+            file_path_1 = os.path.join(app.config['UPLOAD_FOLDER'], filename_1)
+            file_1.save(file_path_1)
+
             file_2 = request.files["file_2"]
+            filename_2 = secure_filename(file_2.filename)
+            file_path_2 = os.path.join(app.config['UPLOAD_FOLDER'], filename_2)
+            file_2.save(file_path_2)
+            # print('HERE:',file_1)
             outputpath = request.form["outputpath"]
-            df1=pd.read_csv(file_1.filename)
-            df2=pd.read_csv(file_2.filename)
-            data_to_save, file_name = scopingRec(df1,df2)
-            # for data in data_to_save:
-                # Update outputs folder to be an option for user
-            filepath=os.path.join(outputpath,file_name)                
-            data_to_save.to_csv(filepath+'.csv', index=False)
-                # file.save(os.path.join("outputs",filename))
+            df1=pd.read_csv(file_path_1)
+            removeLocalFile(file_path_1)
+            df2=pd.read_csv(file_path_2)
+            removeLocalFile(file_path_2)
+            data_to_save, output_file_name = scopingRec(df1, df2)
+            output_filepath=os.path.join(outputpath,output_file_name)                
+            data_to_save.to_csv(output_filepath+'.csv', index=False)
+            # file.save(os.path.join("outputs",filename))
             flash(f'Success! Files uploaded to {outputpath} folder', 'success')
             return render_template("scoping.html", message="Success!")
         except Exception as e:
