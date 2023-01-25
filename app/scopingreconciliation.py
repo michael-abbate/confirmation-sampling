0ad45fbf71be8db4dba228b88c05a43353a22339
Michael Abbate <mabbate@mabba-NY-1022m.local>
2023-01-24 23:16:40 -0500

    add USD sum and profit center blank check

--- app/scopingreconciliation.py
+++ app/scopingreconciliation.py
@@ -1,17 +1,35 @@
 import pandas as pd
 import os
 
+def checkForBlank(col_data):
+    if "" in col_data:
+        return "YES"
+    else:
+        return "NO"
+
 def scopingRec(scope_file1, scope_file2):
     cols_to_join = ['Profit Center 0PROFIT_CTR','Company Code 0COMP_CODE','Functional Area 0FUNC_AREA']
+    
     df1 = pd.read_csv(scope_file1)
     print("File 1 Row Count:", df1.shape[0])
+    df1_usd = df1["USD Equivalent (at EOD) LIQ_PO990"].sum()
+    print("File 1 USD Sum:", df1_usd)
     df1 = df1.astype(str)
-    print(df1.dtypes)
+    df1_profit_centers = df1['Profit Center 0PROFIT_CTR'].unique()
+    print("Are there blank profit centers in file 1:", checkForBlank(df1_profit_centers))
+    # print(df1.dtypes)
+    print()
+
     df2 = pd.read_csv(scope_file2)
     print("File 2 Row Count:", df2.shape[0])
+    df2_usd = df2["USD Equivalent (at EOD) LIQ_PO990"].sum()
+    print("File 2 USD Sum:", df2_usd)
     df2 = df2.astype(str)
+    df2_profit_centers = df2['Profit Center 0PROFIT_CTR'].unique()
+    print("Are there blank profit centers in file 2:", checkForBlank(df2_profit_centers))
+    # print()
+    # print(df2.dtypes)
     print()
-    print(df2.dtypes)
                      
     df2 = df2.loc[(df2[cols_to_join[0]].isin(df1[cols_to_join[0]].unique()))
                 &   (df2[cols_to_join[1]].isin(df1[cols_to_join[1]].unique()))  
